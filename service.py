dea767ba6292fa6771089252854c7fdd6b78f38f
rajat singla <rajat@stck.me>
2025-01-19 16:14:41 +0530

    setup secure session db setup

--- service.py
+++ service.py
@@ -19,9 +19,18 @@ def make_app():
     )
 
     api_factory = APIFactory(router, urls_prefix=settings.URL_PREFIX)
-
+       
+    sessiondb_conn = dict(
+        host=settings.SESSIONSDB_HOST,
+        port=settings.SESSIONSDB_PORT,
+        password=settings.SESSIONSDB_PASSWD,
+        db=settings.SESSIONSDB_NO,
+    )
+    api_factory.setup_session_db(sessiondb_conn)
     api_factory.setup_db_transaction(db)
     setup_routes(api_factory)
+    # either use peewee or db.evolve
+    # peewee.setup_db_from_basemodel(db, CommonModel)
     db.evolve(interactive=False)
 
 
